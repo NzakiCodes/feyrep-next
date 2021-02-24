d3df712c37a9b994b0d0e31e3ad936a51aa4086c
Nzaki Michael Ekere <ekisnzaki@gmail.com>
2021-02-24 21:17:17 +0100

    added ne whero image'

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,6 @@
 const Header = ()=>(
     <div className="min-w-full bg-cover bg-center ">
-        <img className="fw-header-hero w-screen h-auto" src="/images/img-hero.jpg" alt=""/>
+        <img className="fw-header-hero " src="/images/img-hero.jpg" alt=""/>
     </div>
 );
 
