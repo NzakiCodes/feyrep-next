125772b3d06dd5e6503358712f94188170f4b878
Nzaki Michael Ekere <ekisnzaki@gmail.com>
2021-02-25 06:06:13 +0100

    fixed hero image bug

--- components/header.jsx
+++ components/header.jsx
@@ -1,6 +1,8 @@
+
+
 const Header = ()=>(
     <div className="min-w-full bg-cover bg-center ">
-        <img className="fw-header-hero " src="/images/img-hero.jpg" alt=""/>
+        <img className="fw-header-hero " src="/images/img-hero.JPG" alt=""/>
     </div>
 );
 
