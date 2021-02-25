ee72bca76cc4eec8f608425f32018cede7755cbf
Nzaki Michael Ekere <ekisnzaki@gmail.com>
2021-02-25 23:08:56 +0100

    Change button

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,9 @@
 
 const Header = () => (
     <div class="relative h-screen ">
-        <div className="absolute inset-0 w-full h-full object-cover object-center z-30 bg-opacity-70 bg-primary "></div>
+        <div className="absolute inset-0 w-full h-full object-cover object-center z-30 bg-opacity-70 bg-primary ">
+            
+        </div>
         <img class="absolute inset-0 w-full h-full object-cover object-center z-10" src="/images/img-hero.JPG" alt="WomanBeach" />
     </div>
 );
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,16 +1,16 @@
 const Navbar = ({activeBtn}) => {
     return (
-        <div className="z-50 fixed w-full bg-white border-white border-b-2 shadow">
+        <div className="z-50 fixed w-full bg-white   shadow-lg">
             {/* <h1 className="text font-medium text-2xl">FEYReP</h1> */}
             <div className="bg-primary py-5 px-12 hidden">
             </div>
             <div className=" flex flex-row justify-between lg:justify-center fw-container sm:gap-10 md:gap-0" >
                 <div className="py-3 px-4  md:px-6 flex">
-                    <div className="col-span-5 xlg:col-span-4 p-2">
+                    <div className="col-span-5 xlg:col-span-4 p-1 ">
                         <img className="fw-logo" src="/images/logo-icon.png" alt="FEYReP logo" />
                     </div>
                     <div className="col-span-8">
-                        <h1 className="fw-logo-text">FEY<span className="text-gray-500">ReP</span></h1>
+                        <h1 className="fw-logo-text text-3xl m-auto">FEY<span className="text-gray-500">ReP</span></h1>
                     </div> 
                 </div>
                 <div className="hidden lg:block  ">
@@ -25,8 +25,11 @@ const Navbar = ({activeBtn}) => {
                     </div>
                 </div>
                 <div className=" hidden xl:block xlg:col-span-1">
-                    <div className=" my-8 mx-4 px-4">
-                        <a href="/donate" className="fw-donate-btn hover:text-secondary-200 hover:border-secondary-200">Donate Now</a>
+                    <div className="ml-12">
+                        <a href="/donate" className="px-14 py-8 inline-block hover:scale-90 transform transition bg-primary text-white hover:bg-primary-200">
+                            {/* <span className=" inline-block hover:scale-150 transform transition"></span> */}Donate Now 
+                        </a>
+                        {/* <a href="/donate" className="fw-donate-btn hover:text-secondary-200 hover:border-secondary-200">Donate Now</a> */}
                     </div>
                 </div>
                 <div className="sm:col-span-7 lg:hidden ">
