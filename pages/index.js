ec0abac8d1c67a5ef057668407b74983b9a1c535
Nzaki Michael Ekere <ekisnzaki@gmail.com>
2021-04-04 06:24:13 +0100

    ff

--- pages/index.js
+++ pages/index.js
@@ -78,6 +78,27 @@ export default function Home({ header }) {
           <Section fullWidth className="container lg:mb-14 lg:relative lg:h-80 ">
             <InfoCard />
           </Section>
+          <Section className="container">
+            <Grid className="grid-cols-2 gap-x-7">
+              <div className="relative h-96">
+                <div className="">
+                  <img className="rounded-lg" src="/images/sections/GBV_001.JPG" alt="Children" />
+                </div>
+              </div>
+              <div>
+                <div className="max-w-lg py-7 px-4 text-left">
+                  <span className="bg-primary-600 block  mt-0 mb-0 px-10 w-4" style={{ padding: "1px 2px 1px 35px" }}></span>
+                  <span className="bg-primary-600 block  mb-6 mt-1 px-10 w-5" style={{ padding: "1px 2px 1px 70px" }}></span>
+                  <h3 className=" text-5xl font-bold  mb-2 pb-0.5 ">Gender Based Violence (GBV).</h3>
+                  <p className="text-xl font-normal font-quicksand text-gray-800">Despite the global outrage against Gender Based Violence and its attendant physical, emotional and psychological consequences on survivors, their families and the larger community...</p>
+                  <div className="py-5 mt-2">
+                    <a href="/donate" className="px-12 font-quicksand py-3 inline-block rounded-full hover:bg-transparent text-white hover:text-primary-200 font-bold text-md uppercase transition border-primary-200  bg-primary-200 border-2">Know More.</a>
+                  </div>
+                </div>
+              </div>
+            </Grid>
+
+          </Section>
           <Section fullHeight fullWidth className="bg-white my-14" style={{ overflow: "hidden" }}>
             <Slider {...sliderSettings} className="grid sm:grid-flow-col md:grid-flow-row grid-cols-1 md:grid-cols-3  relative gap-0 w-full">
               <NewsFlashCard img={"/images/img-hero.JPG"}>
@@ -110,15 +131,26 @@ export default function Home({ header }) {
           </Section>
 
           <Section fullWidth fullHeight className="" style={{ background: "url(/images/sections/children-3319460_1920.jpg) center no-repeat fixed", backgroundSize: "cover" }}>
-            <div className="w-full h-full py-14 lg:px-10" style={{ background: "rgba(0,0,0,0.89)" }}>
-            <Grid className="grid-cols-2 gap-x-4">
-              <div className="rounded-lg">
-                <img className="rounded-lg" src="/images/sections/children-3319460_1920.jpg" alt="Children" />
-              </div>
-              <div>
-              {/* <img src="/images/sections/children-3319460_1920.jpg" alt="Children" /> */}
-              </div>
-            </Grid>
+            <div className="w-full h-full py-14 lg:px-24" style={{ background: "rgba(0,0,0,0.89)" }}>
+              <Grid className="grid-cols-2 gap-x-7">
+                <div className="rounded-lg">
+                  <img className="rounded-lg" src="/images/sections/IMG-2014_b.jpg" alt="Children" />
+                </div>
+                <div>
+                  <div className="max-w-lg py-6 px-4 text-left">
+                    <span className="bg-secondary-200 block  mt-0 mb-0 px-10 w-1" style={{ padding: "1px 2px 1px 15px" }}></span>
+                    <span className="bg-secondary-200 block  mb-6 mt-1 px-10 w-2" style={{ padding: "1px 2px 1px 30px" }}></span>
+                    <h3 className=" text-6xl font-bold  mb-2 pb-0.5 text-white">Can Poverty be Eradicated?</h3>
+                    <p className="text-2xl font-normal font-sans text-white">Poverty is a problem that has proven solutions, and donations play a crucial role in it. </p>
+                    <div className="py-3 mt-1">
+                      <a href="/donate" className="px-12 py-4 inline-block rounded-full bg-transparent hover:text-gray-700 font-bold text-md uppercase transition hover:bg-secondary font-quicksand text-white border-secondary-200 border-2">
+                        {/* <span className=" inline-block hover:scale-150 transform transition"></span> */}Donate Now
+                        </a>
+                      {/* <a href="/donate" className="fw-donate-btn hover:text-secondary-200 hover:border-secondary-200">Donate Now</a> */}
+                    </div>
+                  </div>
+                </div>
+              </Grid>
             </div>
           </Section>
           <Section fullWidth fullHeight className="py-10 px-14 lg:px-14  my-14" style={{ background: "url(/images/sections/events_bg.png) top left no-repeat bg-contain", backgroundSize: "contain" }}>
@@ -131,13 +163,23 @@ export default function Home({ header }) {
               <Card border img={img1} title="Bright Future for responsible boys." date="January 3, 2019" description="This is FEYReP's Advocacy Programme for the boy child. It is targetted at sensitizing the boy child on the dangers of drug... " />
             </Grid>
           </Section>
+          <Section fullWidth fullHeight>
+            <Grid className="grid-cols-1 lg:grid-cols-2 ">
+              <div className="">
+                <img src="/images/sections/children-3319460_1920.jpg" alt="Children" />
+              </div>
+              <div>
+                <img src="/images/sections/children-3319460_1920.jpg" alt="Children" />
+              </div>
+            </Grid>
+          </Section>
           <Section >
             <Grid className="grid-cols-2 gap-x-4">
               <div className="rounded-lg">
                 <img className="rounded-lg" src="/images/sections/children-3319460_1920.jpg" alt="Children" />
               </div>
               <div>
-              <img src="/images/sections/children-3319460_1920.jpg" alt="Children" />
+                <img src="/images/sections/children-3319460_1920.jpg" alt="Children" />
               </div>
             </Grid>
           </Section>
@@ -193,7 +235,7 @@ export default function Home({ header }) {
             </div>
 
           </Section>
-         
+
         </div>
       </main>
 
