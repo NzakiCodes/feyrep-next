6a620b6ebb2ae93e07156a22e762b6f3ab274ee1
Nzaki Michael <ekisnzaki@gmail.com>
2021-08-05 21:13:21 +0100

    No mmore Suicide

--- pages/news/[id].js
+++ pages/news/[id].js
@@ -28,38 +28,5 @@ const NewsItem = function ({ data }) {
 }
 
 
-// async function postData(url = '', data = {}) {
-//     // Default options are marked with *
-//     const response = await fetch(url);
-//     return response.json(); // parses JSON response into native JavaScript objects
-// }
-// export async function getStaticPaths() {
-
-//     const res = await fetch("https://www.master-7rqtwti-dd2fyzz46gjlw.ca-1.platformsh.site/items/news");
-//     const posts = await res.json();
-
-//     const paths = posts.data.map((post) => ({
-//         params: { id: post.id.toString() },
-//     }));
-//     return { paths, fallback: false }
-// }
-
-// export async function getStaticProps({ params }) {
-//     const res = await fetch(`https://www.master-7rqtwti-dd2fyzz46gjlw.ca-1.platformsh.site/items/news/${params.id}`);
-//     const data = await res.json();
-
-//     if (!data) {
-//         return {
-//             notFound: true,
-//         }
-//     }
-
-//     return {
-//         props: {
-//             data: data.data
-//         }
-//     }
-// }
-
 
 export default NewsItem
